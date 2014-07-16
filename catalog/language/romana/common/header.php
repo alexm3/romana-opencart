95217b5ebc0a49737319f3f2cda9a0ecd66b903f
florinsith <flo@shop-carts.net>
2014-07-16 22:44:39 +0300

    urcare traducere 1.5.6.4

---
+++ catalog/language/romana/common/header.php
@@ -0,0 +1,11 @@
+<?php
+// Text
+$_['text_home']     = 'Prima Pagină';
+$_['text_wishlist'] = 'Wish List (%s)';
+$_['text_shopping_cart']     = 'Coş de cumpărături';
+$_['text_search']   = 'Căutare'; 
+$_['text_welcome']  = 'Salut, îți poți crea un <a href="%s">cont nou</a> sau te poți <a href="%s">autentifica</a>.';
+$_['text_logged']   = 'Esti logat ca <a href="%s">%s</a> <b>(</b> <a href="%s">Delogare</a> <b>)</b>';
+$_['text_account']  = 'Contul meu';
+$_['text_checkout'] = 'Comandă';
+?>
