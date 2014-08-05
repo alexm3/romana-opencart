78c5f2e5851016a819bb3ee9d0bce9fba1783ac1
florinsith <flo@shop-carts.net>
2014-08-05 19:39:10 +0300

    actualizare romana.php din catalog

--- catalog/language/romana/romana.php
+++ catalog/language/romana/romana.php
@@ -1,63 +1,69 @@
 <?php
-// Locale
+// Localizare
 $_['code']                  = 'ro';
 $_['direction']             = 'ltr';
 $_['date_format_short']     = 'd.m.Y';
 $_['date_format_long']      = 'l dS F Y';
 $_['time_format']           = 'h:i:s A';
+$_['datetime_format']       = 'd/m/Y H:i:s';
 $_['decimal_point']         = ',';
 $_['thousand_point']        = '.';
 
 // Text
-$_['text_home']             = 'Prima Pagină';
+$_['text_home']             = '<i class="fa fa-home"></i>';
 $_['text_yes']              = 'Da';
 $_['text_no']               = 'Nu';
 $_['text_none']             = ' --- Nimic --- ';
 $_['text_select']           = ' --- Selectaţi --- ';
 $_['text_all_zones']        = 'Toate Zonele';
-$_['text_pagination']       = 'Afişare {start} - {end} din {total} ({pages} Pagini)';
-$_['text_separator']        = ' &gt; ';
+$_['text_pagination']       = 'Afişare %d - %d din %d (%d pagini)';
+$_['text_loading']          = 'Încărcare...';
 
-// Buttons
-$_['button_add_address']    = 'Adaugă Adresă';
+// Butoane
+$_['button_address_add']    = 'Adaugă Adresă';
 $_['button_back']           = 'Înapoi';
 $_['button_continue']       = 'Continuare';
 $_['button_cart']           = 'Adaugă în Coş';
-$_['button_compare']        = 'Compară produs';
+$_['button_cancel']         = 'Anulează';
+$_['button_compare']        = 'Compară produsul';
 $_['button_wishlist']       = 'Adaugă in Wishlist';
-$_['button_checkout']       = 'Comandă';
-$_['button_confirm']        = 'Confirmare Comandă';
+$_['button_checkout']       = 'Plasează comanda';
+$_['button_confirm']        = 'Confirmă comanda';
 $_['button_coupon']         = 'Aplicare cupon';
-$_['button_delete']         = 'Ştergere';
+$_['button_delete']         = 'Şterge';
 $_['button_download']       = 'Descarcă';
-$_['button_edit']           = 'Editare';
-$_['button_filter']         = 'Redefiniți Căutarea';
+$_['button_edit']           = 'Editează';
+$_['button_filter']         = 'Filtrează căutarea';
 $_['button_new_address']    = 'Adresă nouă';
 $_['button_change_address'] = 'Schimbă adresa';
 $_['button_reviews']        = 'Opinii';
-$_['button_write']          = 'Spune-ţi opinia';
+$_['button_write']          = 'Scrie-ţi opinia';
 $_['button_login']          = 'Autentificare';
 $_['button_update']         = 'Actualizare';
 $_['button_remove']         = 'Eliminare';
 $_['button_reorder']        = 'Comandă din nou';
 $_['button_return']         = 'Returnare';
-$_['button_shopping']       = 'Continuă cumparaturile';
+$_['button_shopping']       = 'Continuă cumpărăturile';
 $_['button_search']         = 'Căutare';
-$_['button_shipping']       = 'Aplicare Livrare';
+$_['button_shipping']       = 'Aplică metoda de livrare';
 $_['button_guest']          = 'Comandă fără cont';
 $_['button_view']           = 'Vezi';
 $_['button_voucher']        = 'Aplicare Voucher';
-$_['button_upload']         = 'Încărcare Fişier';
-$_['button_reward']         = 'Aplicare Puncte';
+$_['button_upload']         = 'Încărcă un fişier';
+$_['button_reward']         = 'Aplicare puncte de recompensă';
 $_['button_quote']          = 'Estimare';
+$_['button_list']           = 'Listă';
+$_['button_grid']           = 'Grilă';
+$_['button_map']            = 'Vezi harta google';
 
-// Error                               
+// Erori
+$_['error_exception']       = 'Cod de eroare(%s): %s În %s pe linia %s';                              
 $_['error_upload_1']        = 'Atenţie: Fisierul uploadat depaseste marimea maxima setata in php.ini!';
 $_['error_upload_2']        = 'Atenţie: Fisierul uploadat depaseste marimea maxima specificata in formularul HTML!';
 $_['error_upload_3']        = 'Atenţie: Fisierul selectat , a fost doar partial uploadat!';
 $_['error_upload_4']        = 'Atenţie: Niciun fisier nu a fost inca uploadat!';
-$_['error_upload_6']        = 'Atenţie: Fisier temporar lipsa!';
-$_['error_upload_7']        = 'Atenţie: Failed to write file to disk!';
+$_['error_upload_6']        = 'Atenţie: Lipseste un director temporar!';
+$_['error_upload_7']        = 'Atenţie: Fisierul nu poate fii scris pe mediul de stocare!';
 $_['error_upload_8']        = 'Atenţie: Uploadarea fisierului oprita de extesnie!';
-$_['error_upload_999']      = 'Atenţie: Niciun cod de eroare valabil!';
+$_['error_upload_999']      = 'Atenţie: Niciun cod de eroare disponibil!';
 ?>
